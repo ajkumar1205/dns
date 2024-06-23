1dd32bcfedf8c58d21507c84da1746b94bcbd48d
Ajay Kumar <ajthakur1205@gmail.com>
2024-06-23 17:41:48 +0530

    Signup page

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,11 +1,25 @@
 import { Elysia, t } from "elysia";
 import { App } from "../../components/App";
 
-const ABC = () => {
-    return <div class="text-center text-red-400 text-3xl">
-        This is the home
+const SignUp = () => {
+    return <div class="flex items-center justify-center">
+        <div>
+            <div>
+                Welcome Master!
+                <div>Verify it's you.</div>
+            </div>
+            <input type="text" />
+            <input type="password" />
+            <div>
+                <span>Let's Go!</span>
+            </div>
+        </div>
     </div>
 }
 
+
+
+
+
 export const home = new Elysia()
-    .get("", () => <App><ABC /></App>)
+    .get("", () => <App><SignUp /></App>)
